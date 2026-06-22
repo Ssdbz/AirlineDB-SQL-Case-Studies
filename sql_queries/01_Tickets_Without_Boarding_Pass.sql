681e5bfb8b60a1769106831b5abcd802b337db10
Souvik Sen <ss.souvikeee@gmail.com>
2026-06-22 07:34:26 +0530

    01_Tickets_Without_Boarding_Pass.sql

--- sql_queries/01_Tickets_Without_Boarding_Pass.sql
+++ sql_queries/01_Tickets_Without_Boarding_Pass.sql
@@ -1,1 +1,17 @@
+/*
+Case Study 1: Tickets Without Boarding Passes
 
+Business Question:
+How many tickets exist without boarding passes?
+
+Concepts Used:
+- LEFT JOIN
+- NULL Handling
+- COUNT()
+*/
+
+SELECT COUNT(DISTINCT t.ticket_no) AS tickets_without_boarding_pass
+FROM tickets t
+LEFT JOIN boarding_passes bp
+ON t.ticket_no = bp.ticket_no
+WHERE bp.boarding_no IS NULL;
